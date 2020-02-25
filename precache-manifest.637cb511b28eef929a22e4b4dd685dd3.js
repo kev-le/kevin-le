cbd10831f3bd67eb27687198fa6e7e04dca25d5c
Kevin Le <kevin_le@sfu.ca>
2020-02-24 22:44:50 -0800

    Updates

---
+++ precache-manifest.637cb511b28eef929a22e4b4dd685dd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213e88a1d822b3f98c92c6b6fd71bf93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20cc136812d0fbf5099a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "821a76f94886880eecbf",
+    "url": "/static/js/2.8780bc2f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8780bc2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cc136812d0fbf5099a",
+    "url": "/static/js/main.1358076f.chunk.js"
+  },
+  {
+    "revision": "d318169894f4cd33f3a3",
+    "url": "/static/js/runtime-main.8da25891.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
